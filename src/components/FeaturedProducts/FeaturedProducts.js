d01af0ffa166acda23475b21ed29e7bfd6f1b607
israelcena <israelsjm@gmail.com>
2020-04-10 20:15:49 -0300

    include alt

--- src/components/FeaturedProducts/FeaturedProducts.js
+++ src/components/FeaturedProducts/FeaturedProducts.js
@@ -10,7 +10,7 @@ const FeaturedProducts = () => {
           <Card className="featured-product h-full" bodyStyle={{ padding: 15 }}>
             <Row>
               <Col xs={4} sm={6} md={6} lg={5} xl={5} className="text-center">
-                <img src={product.logo} alt="" height="40" width="40" className="mt-2" />
+                <img src={product.logo} alt={product.name} height="40" width="40" className="mt-2" />
               </Col>
               <Col xs={20} sm={18} md={18} lg={19} xl={19}>
                 <h3 className="mb-1 text-white">{product.name}</h3>
