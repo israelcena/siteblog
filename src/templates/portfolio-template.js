58848679ec760b27ff320739a24e0efc94aab839
israelcena <israelsjm@gmail.com>
2020-05-10 11:09:28 -0300

    edit page title

--- src/templates/portfolio-template.js
+++ src/templates/portfolio-template.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 import MyPortfolio from '../components/portfolio'
 
 const Portfolio = () => (
-  <Layout title="My Portfolio" className="bg-black">
+  <Layout title="Portfolio" className="bg-black">
     <MyPortfolio />
   </Layout>
 )
